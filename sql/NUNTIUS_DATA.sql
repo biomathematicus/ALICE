a5e1b71c8ddf8316b60b6a50c894724aea1dd949
biomathematicus <biomathematicus@gmail.com>
2021-08-14 01:26:59 -0500

    Initial deployment of data in NUNTIUS table

---
+++ sql/NUNTIUS_DATA.sql
@@ -0,0 +1,125 @@
+USE [ALICE_DEVELOPMENT]
+delete from [dbo].[NUNTIUS]
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'F_BORN', N'F_BORN', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'F_EMAIL', N'F_EMAIL', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'F_LANGUAGE', N'F_LANGUAGE', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'F_LOGIN_NAME', N'F_LOGIN_NAME', 1, CAST(N'2002-12-24T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'F_NAME', N'F_NAME', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'F_PASSWORD', N'F_PASSWORD', 1, CAST(N'2002-12-24T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'F_SEARCH', N'F_SEARCH', 1, CAST(N'2002-12-29T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'F_TAG', N'F_TAG', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'F_TEXT', N'F_TEXT', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'F_TITLE', N'F_TITLE', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_ALL', N'L_ALL', 1, CAST(N'2002-12-26T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_CLASSIC', N'L_CLASSIC', 1, CAST(N'2002-12-26T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_CONTEMPORARY', N'L_CONTEMPORARY', 1, CAST(N'2002-12-26T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_COPYRIGHT', N'L_COPYRIGHT', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_DELETE', N'L_DELETE', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_ESSAY', N'L_ESSAY', 1, CAST(N'2002-12-26T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_FORGOT_PASS', N'L_FORGOT_PASS', 1, CAST(N'2002-12-29T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_FORUM', N'L_FORUM', 1, CAST(N'2003-01-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_LITERATRONIC', N'L_LITERATRONIC', 1, CAST(N'2002-12-24T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_LOGIN', N'L_LOGIN', 1, CAST(N'2002-12-24T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_LOGOUT', N'L_LOGOUT', 1, CAST(N'2002-12-24T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_MORE', N'L_MORE', 1, CAST(N'2002-01-05T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_NEW', N'L_NEW', 1, CAST(N'2003-01-26T23:19:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_NEW_USER', N'L_NEW_USER', 1, CAST(N'2003-01-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_NEXT', N'L_NEXT', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_OLD_USER', N'L_OLD_USER', 1, CAST(N'2003-01-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_PREVIOUS', N'L_PREVIOUS', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_PRIVACY', N'L_PRIVACY', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_REGISTER', N'L_REGISTER', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_SAVE', N'L_SAVE', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_SEARCH', N'L_SEARCH', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_USER', N'L_USER', 1, CAST(N'2003-01-01T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'L_WHY_REGISTER', N'L_WHY_REGISTER', 1, CAST(N'2002-12-24T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'P_HISTORIA', N'P_HISTORIA', 1, CAST(N'2002-12-24T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'P_INDICIUM', N'P_INDICIUM', 1, CAST(N'2002-12-24T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'P_INITIUM', N'P_INITIUM', 1, CAST(N'2002-12-26T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'P_PAGINA', N'P_PAGINA', 1, CAST(N'2003-01-01T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'P_QUAESTIO', N'P_QUAESTIO', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'P_TRACTUS', N'P_TRACTUS', 1, CAST(N'2002-06-13T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'sa_artifex_dtl', N'sa_artifex_dtl', 1, CAST(N'2003-01-26T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'sa_artifex_lst', N'sa_artifex_lst', 1, CAST(N'2003-01-26T17:06:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'sa_menu', N'sa_menu', 1, CAST(N'2003-01-26T17:06:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'sa_nuntius_dtl', N'sa_nuntius_dtl', 1, CAST(N'2003-01-26T17:07:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'sa_nuntius_lst', N'sa_nuntius_lst', 1, CAST(N'2003-01-26T17:07:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'sa_opus_dtl', N'sa_opus_dtl', 1, CAST(N'2003-01-26T17:07:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'sa_opus_lst', N'sa_opus_lst', 1, CAST(N'2003-01-26T17:08:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'sa_pagina_dtl', N'sa_pagina_dtl', 1, CAST(N'2003-01-26T17:08:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'sa_pagina_lst', N'sa_pagina_lst', 1, CAST(N'2003-01-26T17:08:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'sa_reticulum_dtl', N'sa_reticulum_dtl', 1, CAST(N'2003-01-26T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'HTML', N'T_ABOUT', N'T_ABOUT', 1, CAST(N'2002-12-26T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'HTML', N'T_DEFINITION', N'T_DEFINITION', 1, CAST(N'2003-01-18T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'T_META_DESCR', N'T_META_DESCR', 1, CAST(N'2003-01-18T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'T_META_KEYWRD', N'T_META_KEYWRD', 1, CAST(N'2003-01-18T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'HTML', N'T_NEWS', N'T_NEWS', 1, CAST(N'2003-01-18T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'HTML', N'T_PRIVACY', N'T_PRIVACY', 1, CAST(N'2002-12-26T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'HTML', N'T_WHY_REGISTER', N'T_WHY_REGISTER', 1, CAST(N'2002-12-29T00:00:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'TAB_INTRO', N'TAB_INTRO', 1, CAST(N'2016-02-09T22:13:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'TAB_LIVESCRIBE', N'TAB_LIVESCRIBE', 1, CAST(N'2016-02-09T22:13:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'TAB_MEDIA', N'TAB_MEDIA', 1, CAST(N'2016-02-09T22:13:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'TAB_PDF', N'TAB_PDF', 1, CAST(N'2016-02-09T22:13:00' AS SmallDateTime))
+GO
+INSERT [dbo].[NUNTIUS] ([cd_type], [cd_nuntius], [ds_description], [id_created], [dt_created]) VALUES (N'LABEL', N'TAB_WIKI', N'TAB_WIKI', 1, CAST(N'2016-02-09T22:13:00' AS SmallDateTime))
+GO
